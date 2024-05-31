8dd8f0a953cec520a13504fc9575ac98dd283ed9
Ivan22wyh <Ivan22wyh@outlook.com>
2024-05-31 14:42:52 +0800

    1

--- scripts/process_redpajama_data.py
+++
@@ -1,7 +0,0 @@
-from datasets import load_dataset
-
-ds = load_dataset("togethercomputer/RedPajama-Data-V2",
-                  name="default",
-                  partition="head_middle",
-                  snapshots=["2023-06", "2022-49"],
-                  languages=["en"]) 
